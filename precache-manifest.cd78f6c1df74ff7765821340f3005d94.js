f0f1db7242a37c81470321279c4b85f7ab28cfbf
rickafds <rickafds@users.noreply.github.com>
2020-05-10 19:29:19 +0000

    Deploying to gh-pages from  @ 3ed1d7c36ac550eb6edb53c3b5846eb4a8770830 🚀

---
+++ precache-manifest.cd78f6c1df74ff7765821340f3005d94.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d05d8ef2fb6ac387011ef12d144f913c",
+    "url": "/GoBarber/index.html"
+  },
+  {
+    "revision": "5f8b7516e736834af64b",
+    "url": "/GoBarber/static/js/2.2b21b420.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/GoBarber/static/js/2.2b21b420.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5409e02ac750c2dd1e4e",
+    "url": "/GoBarber/static/js/main.09362a82.chunk.js"
+  },
+  {
+    "revision": "6a99791303a96590f775",
+    "url": "/GoBarber/static/js/runtime-main.32a212c6.js"
+  },
+  {
+    "revision": "a49b07ec538713cf0e3910693544c7be",
+    "url": "/GoBarber/static/media/logo.a49b07ec.svg"
+  },
+  {
+    "revision": "6eac314f78fe224c4d769fb646523773",
+    "url": "/GoBarber/static/media/sign-in-background.6eac314f.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.740a9c5d12686c3e2e0c5eb36441398b.js"
+  "/GoBarber/precache-manifest.cd78f6c1df74ff7765821340f3005d94.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/GoBarber/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
