d4ed81fd1c637e197449d058ce669a3497eee746
rickafds <rickafds@users.noreply.github.com>
2020-05-10 19:11:08 +0000

    Deploying to gh-pages from  @ a5e6179370f096a1f69616c37d9b3273d8d8fa0b 🚀

---
+++ static/js/main.7f19ea71.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp06-gobarber-web"]=this["webpackJsonp06-gobarber-web"]||[]).push([[0],{3:function(e,t,n){e.exports=n(8)},8:function(e,t,n){"use strict";n.r(t);var r=n(0),o=n.n(r),a=n(2),l=function(){return o.a.createElement("h1",null,"Hello Word")};n.n(a).a.render(o.a.createElement(o.a.StrictMode,null,o.a.createElement(l,null)),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.7f19ea71.chunk.js.map
